bbf63ac1d58b96c8cc60a2deb65058d728c984fa
poroh-kun <meta.poroh.kun@gmail.com>
2020-08-24 23:30:32 +0300

    add project files

---
+++ ModBusSlave/ModBusSlave.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		ModBusSlave.ino
+ Created:	8/24/2020 11:24:37 PM
+ Author:	Poroh
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop()
+{
+
+}
