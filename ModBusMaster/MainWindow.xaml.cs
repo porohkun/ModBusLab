71348d8ec3530e2be971b8afbf79b11f620fc6b5
poroh-kun <meta.poroh.kun@gmail.com>
2020-08-25 01:10:03 +0300

    master code

--- ModBusMaster/MainWindow.xaml.cs
+++ ModBusMaster/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using NModbus;
+using NModbus.Serial;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,16 +22,100 @@ namespace ModBusMaster
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private Register[] _registers = new Register[10]
+        {
+            new Register(0),
+            new Register(0),
+            new Register(0),
+            new Register(0),
+            new Register(0),
+            new Register(0),
+            new Register(0),
+            new Register(0),
+            new Register(0),
+            new Register(0)
+        };
+
+        public Register[] Registers
+        {
+            get => _registers;
+            set
+            {
+                if (_registers != value)
+                {
+                    _registers = value;
+                    NotifyPropertyChanged(nameof(Registers));
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ReadRegisters_Click(object sender, RoutedEventArgs e)
         {
+            using (var port = GetSerialPort())
+            {
+                var factory = new ModbusFactory();
+                IModbusSerialMaster master = factory.CreateRtuMaster(port);
 
+                byte slaveId = 1;
+                ushort startAddress = 0;
+                ushort numRegisters = 10;
+
+                // read five registers		
+                Registers = master.ReadHoldingRegisters(slaveId, startAddress, numRegisters).Select(r => new Register(r)).ToArray();
+            }
+        }
+
+        private void WriteRegisters_Click(object sender, RoutedEventArgs e)
+        {
+            using (var port = GetSerialPort())
+            {
+                var factory = new ModbusFactory();
+                IModbusSerialMaster master = factory.CreateRtuMaster(port);
+
+                byte slaveId = 1;
+                ushort startAddress = 0;
+
+                master.WriteMultipleRegisters(slaveId, startAddress, Registers.Select(r => r.Value).ToArray());
+            }
+        }
+
+        private SerialPort GetSerialPort()
+        {
+            SerialPort port = new SerialPort("COM4")
+            {
+                BaudRate = 115200,
+                DataBits = 8,
+                Parity = Parity.None,
+                StopBits = StopBits.One
+            };
+            port.Open();
+            return port;
+        }
+    }
+
+    public class Register
+    {
+        public ushort Value { get; set; }
+
+        public Register(ushort value)
+        {
+            Value = value;
         }
     }
 }
